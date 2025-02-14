ac079fd16955af5b37ddecd2fb929d7a31b01adb
Maryum Shakeel <48406@students.riphah.edu.pk>
2025-02-14 14:11:11 +0500

    practice

---
+++ object1.js
@@ -0,0 +1,16 @@
+const object= [
+    {
+        Model: 2012,
+       color: 'Red'
+    },
+    {
+       Model: 2022,
+       color: 'Blue'
+    },
+
+]
+function car(data){
+    console.log(data.age)
+}
+let arr=car.map(data);
+console.log(arr);
---
+++ objects.js
@@ -0,0 +1,16 @@
+const object=[
+    {
+        name:'maryum',
+        age:19
+
+    },
+    {
+        name:'zainab',
+        age:20
+    }
+]
+function data(data){
+    console.log(data)
+}
+let newarr=object.map(data);
+console.log(newarr);
